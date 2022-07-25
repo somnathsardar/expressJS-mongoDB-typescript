2a66abaa6068570f53d52c6fb60a512d08e697de
Somnath Sardar <somnath@rimsys.io>
2022-07-25 18:32:12 +0530

    Setup mongodb connection.

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
+import mongoose from 'mongoose'
 import app from "./src/app"
 
 const PORT:any = process.env.PORT ? process.env.PORT : 8000;
+const uri: string = process.env.DATABASE_URL + '/' + process.env.DATABASE_NAME
 
-app.listen(PORT, () => {
-  console.log("Server started at http://localhost:"+PORT)
+// MongoDB connection
+mongoose.connect(uri).then(()=>{
+  console.log('Database connected')
+  
+  // Start express server.
+  app.listen(PORT, () => {
+    console.log("Server started at http://localhost:"+PORT)
+  })
+}).catch(err => {
+  console.log('Unable to connect to database.')
+  console.log(err.message)
 })
